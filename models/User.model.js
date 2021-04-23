b2f9fc26f594354af893e48a5f4203075dddf9f8
mario sousa <vegetalizado@gmail.com>
2021-04-23 18:04:44 +0100

    adds cloudinary

---
+++ models/User.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+ /*  role: String, //Admin, Reader, Writer, Super */
+});
+
+module.exports = model("User", userSchema);
