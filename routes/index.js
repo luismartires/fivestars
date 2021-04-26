d6e6893b3adf106e3967dada4c46733213190e19
Luis Martires <luis.martires@outlook.pt>
2021-04-26 11:21:35 +0100

    area/settings

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,31 @@ router.get('/', async (req, res) => {
 res.render("index");
 });
 
+/* function requireLogin(req, res, next) {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+} */
+
+/* router.get('/', (req, res, next) => {
+  res.render('index', { user: req.session.currentUser } );
+});
+
+router.get("/private", requireLogin, (req, res) => {
+  res.render("private");
+}); */
+
+router.get("/my-area", async (req, res) => {
+  res.render("auth/my-area");
+});
+
+router.get("/settings", async (req, res) => {
+  res.render("auth/settings");
+});
+
+
+
 module.exports = router;
 
