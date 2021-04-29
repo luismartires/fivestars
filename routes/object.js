e7a8b2d76c802870478fd6fa4b81021cef6b83e8
Mario Sousa <vegetalizado@gmail.com>
2021-04-29 16:58:47 +0100

    merge

--- routes/object.js
+++ routes/object.js
@@ -1,41 +1,36 @@
 const express = require("express");
 const Rating = require("../models/Rating.model");
-const User = require("../models/User.model");
 const router = express.Router();
 const requireLogin = require("../configs/middleware");
 const Favorite = require("../models/Favorite.model");
-
 const imdb = require("imdb-api");
-
-
 
 /* OBJECT-SEARCH */
 
-
-
 router.get("/object-search", async (req, res) => {
-  try{
-  const { theObjectName } = req.query;
+  try {
+    const { theObjectName } = req.query;
 
-  const result = await imdb.search(
-    {
-      name: theObjectName,
-    },
-    {
-      apiKey: process.env.CLIENT_ID,
-    }
-  );
-  let objects = result.results;
-console.log(result)
-  res.render("object-search-results", { objects, theObjectName, user: req.session.currentUser });
-  }catch(e){res.render('error')}
+    const result = await imdb.search(
+      {
+        name: theObjectName,
+      },
+      {
+        apiKey: process.env.CLIENT_ID,
+      }
+    );
+    let objects = result.results;
+    res.render("object-search-results", {
+      objects,
+      theObjectName,
+      user: req.session.currentUser,
+    });
+  } catch (e) {
+    res.render("error");
+  }
 });
 
-
-
 /* DETAILS */
-
-
 
 router.post("/details/:id/rating", requireLogin, async (req, res) => {
   try {
@@ -69,16 +64,16 @@ router.get("/details/:id", async (req, res) => {
   res.render("object-details", { object, user: req.session.currentUser });
 });
 
-
-
 /* FAVORITES */
-
-
 
 router.get("/favorites", async (req, res) => {
   const favorites = await Favorite.find({ user: req.session.currentUser._id });
   const ratings = await Rating.find({ user: req.session.currentUser._id });
-  res.render("favorites", { favorites, ratings, user: req.session.currentUser });
+  res.render("favorites", {
+    favorites,
+    ratings,
+    user: req.session.currentUser,
+  });
 });
 
 router.post("/favorites/:id", async (req, res) => {
@@ -111,11 +106,7 @@ router.post("/favorites/:id", async (req, res) => {
   }
 });
 
-
-
 /* ADD-REVIEW */
-
-
 
 router.get("/review/:id", async (req, res) => {
   const result = await imdb.get(
